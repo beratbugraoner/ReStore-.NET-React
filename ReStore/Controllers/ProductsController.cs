8da017184e284716c2690f013a717fc14ae74a24
beratbugraoner <onerberat0@gmail.com>
2022-06-23 14:54:26 +0300

    Proje Oluşturuldu
    -Proje Oluşturuldu
    -Migration Yapıldı
    -Product Controller yazılmaya başlandı

---
+++ ReStore/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReStore.Data;
+using ReStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext context;
+        public ProductsController(StoreContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts() {
+
+            return await context.Products.ToListAsync();
+           
+        }
+        [HttpGet("{id}")] // api//products//3
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await context.Products.FindAsync(id);
+        }
+
+    }
+}
